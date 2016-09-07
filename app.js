768a70509b8f6d6c0f734632cc886567c2c848db
boswellp <petergboswell@hotmail.com>
2016-09-08 00:56:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -27,6 +27,6 @@ server.post('/api/messages', connector.listen());
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/index.html', function (session) {
+bot.dialog('index.html', function (session) {
     session.send("Hello World");
 });
