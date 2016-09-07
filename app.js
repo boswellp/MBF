6acc62ece939f1b44816dc7a3501d8508b7ba7ca
boswellp <petergboswell@hotmail.com>
2016-09-07 11:22:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -14,8 +14,10 @@ session.send('Hello World');
 // Setup Restify Server
 var server = restify.createServer();
 server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-server.listen(process.env.port || 5000, function () {
-console.log('%s FDICbotmbf listening to %s', server.name, server.url);
+server.port = process.env.port || process.env.PORT || 80;
+server.host = process.env.port || '0.0.0.0';
+server.listen(server.port,server.host, function () {
+       console.log('%s FDICbotmbf listening to %s', server.name, server.url);
 });
 
 server.get('/', restify.serveStatic({
