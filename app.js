7bd94cbe39587778ab27d4157bb3aa6514d9a90c
boswellp <petergboswell@hotmail.com>
2016-09-08 01:10:57 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
-var appId = process.env.MY_APP_ID";
+var appId = process.env.MY_APP_ID;
 var appSecret = process.env.MY_APP_PASSWORD;
 
 // Create bot and add dialogs
