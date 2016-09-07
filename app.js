54fa5808ad4c5c6f8ee9bb290176c2c17be53233
boswellp <petergboswell@hotmail.com>
2016-09-07 03:06:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ session.send('Hello World');
 // Setup Restify Server
 var server = restify.createServer();
 server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-server.listen(process.env.port || 8000, function () {
+server.listen(process.env.port || 80, function () {
 console.log('%s listening to %s', server.name, server.url);
 });
 
