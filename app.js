d1baffed643a8b6633863c6b67dadd372241adc7
boswellp <petergboswell@hotmail.com>
2016-09-07 23:17:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,17 +2,16 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
-//var appId = process.env.MY_APP_ID || "Missing your app ID";
-//var appSecret = process.env.MY_APP_SECRET || "Missing your app secret";
-var appId;
-var appPassword;
+var appId = process.env.MY_APP_ID || "c6076e92-cae8-4b0b-83da-565b83d5c8c6";
+var appPasssword = process.env.MY_APP_PASSWORD || "ReY4ExyPZmPsPdqqA3DdGU8";
 
 // Create bot and add dialogs
 var bot = new builder.BotConnectorBot
-({appId: "c6076e92-cae8-4b0b-83da-565b83d5c8c6", appPassword: "ReY4ExyPZmPsPdqqA3DdGU8"});
+({appId: process.env.MY_APP_ID, appPassword: process.env.MY_APP_PASSWORD});
 bot.add('/', new builder.SimpleDialog( function (session) {
 session.send('Hello World');
 }));
+
 
 // Setup Restify Server
 var server = restify.createServer();
