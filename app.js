515680a228995906a9e20413116262eaeff95a15
boswellp <petergboswell@hotmail.com>
2016-09-12 23:53:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var connector = new builder.ChatConnector({
     appPassword: process.env.MY_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
+var arr = []; 
 
 
 var server = restify.createServer();
@@ -20,15 +21,58 @@ server.listen(server.port,server.host, function () {
 //    session.send("Hello. We have only recently (10 September 2016) started development of the FIDICbot for the Microsoft platform for integration with Skype and hopefully KiK. Meanwhile, a fully developed FIDICbot for the FIDIC contracts runs @FIDICbot on Telegram, Messenger, LINE, SMS (+41 79 807 1730) and web (fidic.pw). For these platforms the bot uses Smooch as the integrator. Please check back later or use another messenger service.");
 //});
 
-bot.dialog('/', [  
-    function(session)  
-    {  
-        builder.Prompts.text(session, 'Hello. What is your name?');  
+function getBooksData(key) {  
+    https.get("https://www.googleapis.com/books/v1/volumes?q=" + key + "&maxResults=5", function(res) {  
+        var d = '';  
+        var i;  
+        arr = [];  
+        res.on('data', function(chunk) {  
+            d += chunk;  
+        });  
+        res.on('end', function() {  
+            var e = JSON.parse(d);  
+            for (i = 0; i < e.items.length; i++) {  
+                console.log(i + 1 + ":" + e.items[i].volumeInfo.title);  
+                arr.push({  
+                    "description": e.items[i].volumeInfo.description,  
+                    "title": e.items[i].volumeInfo.title,  
+                    "saleability": e.items[i].saleInfo.saleability,  
+                    "price": e.items[i].saleInfo.listPrice  
+                });  
+            }  
+        });  
+    });  
+}  
+var intents = new builder.IntentDialog();  
+bot.dialog('/', intents);  
+intents.matches(/^Hi/i, [  
+    function(session) {  
+        builder.Prompts.text(session, 'Hey, I am a BookBot. Welcome to Book Searching through Chat!.To start, which books you would like to search?');  
     },  
     function(session, results) {  
-        session.send('Hello %s! How are you today?', results.response);  
+        session.send('Here are books for topic - %s.', results.response);  
+        var b = [];  
+        getBooksData(results.response);  
+    },  
+    function(session) {  
+        builder.Prompts.text(session, 'which books');  
     }  
-]);
+]);  
+intents.matches(/^info?/i, [  
+    function(session) {  
+        builder.Prompts.choice(session, "Which book's info you need?", "1|2|3|4|5");  
+    },  
+    function(session, results) {  
+        var book = arr[results.response.entity - 1];  
+        if (book.saleability == 'FOR_SALE') {  
+            session.send('Title:' + book.title + " Price:" + book.price.amount + " " + book.price.currencyCode);  
+        } else {  
+            session.send('Title:' + book.title + " Price: NOT FOR SALE");  
+        }  
+        session.send('Description:' + book.description);  
+    }  
+]);  
+intents.onDefault(builder.DialogAction.send('Hello. How can I help you today?')); 
 
 server.get('/', restify.serveStatic({
     directory: __dirname,
