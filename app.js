9999af4ff125f0504ef67bd23a528c3c1de8dd0e
boswellp <petergboswell@hotmail.com>
2016-09-12 23:50:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
-/*-----------------------------------------------------------------------------
-A simple "Hello World" bot for the Microsoft Bot Framework. 
------------------------------------------------------------------------------*/
-
 var restify = require('restify');
-//var builder = require('../../core/');
 var builder = require('botbuilder');
 
-//=========================================================
-// Bot Setup
-//=========================================================
-
-
-  
-// Create chat bot
 var connector = new builder.ChatConnector({
     appId: process.env.MY_APP_ID,
     appPassword: process.env.MY_APP_PASSWORD
@@ -20,11 +8,6 @@ var connector = new builder.ChatConnector({
 var bot = new builder.UniversalBot(connector);
 
 
-//=========================================================
-// Bots Dialogs
-//=========================================================
-
-// Setup Restify Server
 var server = restify.createServer();
 server.post('/api/messages', connector.listen());
 server.port = process.env.port || process.env.PORT || 80;
@@ -33,9 +16,19 @@ server.listen(server.port,server.host, function () {
        console.log('%s FDICbotmbf listening to %s', server.name, server.url);
 });
 
-bot.dialog('/', function (session) {
-    session.send("Hello. We have only recently (10 September 2016) started development of the FIDICbot for the Microsoft platform for integration with Skype and hopefully KiK. Meanwhile, a fully developed FIDICbot for the FIDIC contracts runs @FIDICbot on Telegram, Messenger, LINE, SMS (+41 79 807 1730) and web (fidic.pw). For these platforms the bot uses Smooch as the integrator. Please check back later or use another messenger service.");
-});
+//bot.dialog('/', function (session) {
+//    session.send("Hello. We have only recently (10 September 2016) started development of the FIDICbot for the Microsoft platform for integration with Skype and hopefully KiK. Meanwhile, a fully developed FIDICbot for the FIDIC contracts runs @FIDICbot on Telegram, Messenger, LINE, SMS (+41 79 807 1730) and web (fidic.pw). For these platforms the bot uses Smooch as the integrator. Please check back later or use another messenger service.");
+//});
+
+bot.dialog('/', [  
+    function(session)  
+    {  
+        builder.Prompts.text(session, 'Hello. What is your name?');  
+    },  
+    function(session, results) {  
+        session.send('Hello %s! How are you today?', results.response);  
+    }  
+]);
 
 server.get('/', restify.serveStatic({
     directory: __dirname,
