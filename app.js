4cf0ecc2bd2f7a1d69e8635495fc0c926b0be705
boswellp <petergboswell@hotmail.com>
2016-09-12 23:46:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,43 @@
-var builder = require('botbuilder');  
-var connector = new builder.ConsoleConnector().listen();  
-var bot = new builder.UniversalBot(connector);  
-bot.dialog('/', function(session)  
-{  
-    session.send("Hi there, How can I help you?");  
+/*-----------------------------------------------------------------------------
+A simple "Hello World" bot for the Microsoft Bot Framework. 
+-----------------------------------------------------------------------------*/
+
+var restify = require('restify');
+//var builder = require('../../core/');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: process.env.MY_APP_ID,
+    appPassword: process.env.MY_APP_PASSWORD
 });
+var bot = new builder.UniversalBot(connector);
+
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.post('/api/messages', connector.listen());
+server.port = process.env.port || process.env.PORT || 80;
+server.host = process.env.port || '0.0.0.0';
+server.listen(server.port,server.host, function () {
+       console.log('%s FDICbotmbf listening to %s', server.name, server.url);
+});
+
+bot.dialog('/', function (session) {
+    session.send("Hello. We have only recently (10 September 2016) started development of the FIDICbot for the Microsoft platform for integration with Skype and hopefully KiK. Meanwhile, a fully developed FIDICbot for the FIDIC contracts runs @FIDICbot on Telegram, Messenger, LINE, SMS (+41 79 807 1730) and web (fidic.pw). For these platforms the bot uses Smooch as the integrator. Please check back later or use another messenger service.");
+});
+
+server.get('/', restify.serveStatic({
+    directory: __dirname,
+    default: '/index.html'
+}));
