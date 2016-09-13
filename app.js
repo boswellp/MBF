b0fa2bba81cd96bd3ca4548cdecdd662cfddfa1f
boswellp <petergboswell@hotmail.com>
2016-09-14 00:43:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -57,11 +57,12 @@ var jsonData = JSON.parse(data);
 
 for (var i = 0; i < jsonData.clauses.length; i++) {
     var clause = jsonData.clauses[i];
-    arr0.push({ "clauseNumber":clause.clauseNumber });
-    arr1.push({ "clauseTitle":clause.clauseTitle });
+    //arr0.push({ "clauseNumber":clause.clauseNumber });
+    //arr1.push({ "clauseTitle":clause.clauseTitle });
+    arr0.push({ clause.clauseNumber });
+    arr1.push({ clause.clauseTitle });
     console.log("arr0 = " + arr0[i] + "; arr1 = " + arr0[i]); 
-    //arr0.push({ clause.clauseNumber });
-    //arr1.push({ clause.clauseTitle });
+
 
     }
     
