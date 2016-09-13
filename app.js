04d0c0656fa9631ed73ec209cc36ef30c337d0d6
boswellp <petergboswell@hotmail.com>
2016-09-14 01:48:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -79,10 +79,13 @@ intents.matches(/^Hi/i, [
         builder.Prompts.text(session, 'Clause number?');  
     },  
     function(session, results) {  
-        session.send('Books for topic - %s - are available. Submit "info" to choose.', results.response);  
-        var b0 = []; 
-        var b1 = []; 
-        getBooksData1(results.response);  
+        //session.send('Books for topic - %s - are available. Submit "info" to choose.', results.response);  
+        //var b0 = []; 
+        //var b1 = []; 
+        getBooksData1(results.response); 
+        var book = clauseTitleFound[0];
+        console.log("clauseTitleFound = " + book.clauseTitle); 
+        session.send('Clause title is:' + book.clauseTitle); 
     }  
   
 ]);  
