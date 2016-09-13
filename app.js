003975a0da6f7f4a23b39033a333699a5d163520
boswellp <petergboswell@hotmail.com>
2016-09-13 14:20:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -217,14 +217,13 @@ bot.dialog('/prompts', [
 
 bot.dialog('/picture', [
     function (session) {
-        //session.send("Books on which topic do you want?");
-        builder.Prompts.text(session, 'Books on which topic do you want?');
+        session.send("You can easily send pictures to a user...");
         var msg = new builder.Message(session)
             .attachments([{
                 contentType: "image/jpeg",
                 contentUrl: "http://www.theoldrobots.com/images62/Bender-18.JPG"
             }]);
-        //session.endDialog(msg);
+        session.endDialog(msg);
     }
 ]);
 
