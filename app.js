cfd54a60753ef4205efc06ef9d079c95316e3e35
boswellp <petergboswell@hotmail.com>
2016-09-14 16:28:21 +0200

    Update app.js

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ bot.dialog('/', [
         var msg = new builder.Message(session).attachments([card]);
         session.send(msg);
         session.send("Start a search anytime.");
-        session.beginDialog('/contract');
+        session.beginDialog('/start');
     },
     
     //function (session, args, next) {
