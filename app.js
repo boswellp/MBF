7d7585836c76cef592b13335a3aa28df52e6aa72
boswellp <petergboswell@hotmail.com>
2016-09-14 16:31:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -80,6 +80,15 @@ bot.dialog('/', [
 
 //bot.dialog('/help', [function (session) {session.endDialog("Prompts available anytime:\n\n* select - Select a contract. \n* start - Start a search.\n* end - End this conversation.\n* help - Display these prompts.");}]);
 
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
 
 bot.dialog('/start', [
     function (session, args, next) {
@@ -94,15 +103,7 @@ bot.dialog('/start', [
     }
 ]);
 
-bot.dialog('/profile', [
-    function (session) {
-        builder.Prompts.text(session, 'Hi! What is your name?');
-    },
-    function (session, results) {
-        session.userData.name = results.response;
-        session.endDialog();
-    }
-]);
+
 
 
 
