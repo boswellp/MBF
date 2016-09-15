066c91089b87c16334772c19cb11b4de20a764c2
boswellp <petergboswell@hotmail.com>
2016-09-16 00:39:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
+
 var restify = require('restify');
 var https = require('https');
 var builder = require('botbuilder');
+
+const _ = require('lodash');
+const scriptRulesClauses = require('./scriptClauses.json');
+const scriptRulesClausesPlant = require('./scriptClausesPlant.json');
+const scriptRulesIndex = require('./scriptIndex.json');
+const scriptRulesIndexPlant = require('./scriptIndexPlant.json');
 
 var connector = new builder.ChatConnector({
     appId: process.env.MY_APP_ID,
     appPassword: process.env.MY_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
-var arr = []; 
-
+var iFound = 0;
+var clauseTitleFound = [];
 
 var server = restify.createServer();
 server.post('/api/messages', connector.listen());
@@ -18,30 +25,37 @@ server.listen(server.port,server.host, function () {
        console.log('%s FDICbotmbf listening to %s', server.name, server.url);
 });
 
+function getData(session,key) {
+  
+session.userData.result = '';
+clauseTitleFound = [];
+var data = "";
+if (session.userData.name == 'c')
+        if (session.userData.type == 'n')
+            {data = scriptRulesClauses;}
+            else {data = scriptRulesIndex;}
+            
+if (session.userData.name == 'p')
+        if (session.userData.type == 'n')
+            {data = scriptRulesClausesPlant;}
+            else {data = scriptRulesIndexPlant;}
 
 
-function getBooksData(key) {  
-    https.get("https://www.googleapis.com/books/v1/volumes?q=" + key + "&maxResults=5", function(res) {  
-        var d = '';  
-        var i;  
-        arr = [];  
-        res.on('data', function(chunk) {  
-            d += chunk;  
-        });  
-        res.on('end', function() {  
-            var e = JSON.parse(d);  
-            for (i = 0; i < e.items.length; i++) {  
-                console.log(i + 1 + ":" + e.items[i].volumeInfo.title);  
-                arr.push({  
-                    "description": e.items[i].volumeInfo.description,  
-                    "title": e.items[i].volumeInfo.title,  
-                    "saleability": e.items[i].saleInfo.saleability,  
-                    "price": e.items[i].saleInfo.listPrice  
-                });  
-            }  
-        });  
-    });  
-}  
+var clausesAry = [];
+for (var i in data)
+    {clausesAry.push([i, data [i]]);}
+var iFound = 0;
+for (var i = 0; i < clausesAry.length; i++) {
+  if (clausesAry[i][0] == key.toUpperCase()) {
+    iFound = i;
+    break;}
+}
+
+if (iFound != 0){clauseTitleFound[0] = clausesAry[iFound][1];}
+     else {clauseTitleFound[0] = 'notFound';}
+     }
+     
+     
 var intents = new builder.IntentDialog();  
 bot.dialog('/', intents);  
 intents.matches(/^Hi/i, [  
