a60ec148593b3deaeec888e5b284ae02a1b38ef6
boswellp <petergboswell@hotmail.com>
2016-09-15 02:22:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -144,7 +144,7 @@ intents.matches(/^search/i, [
 intents.matches(/^change/i, [
     function (session) {builder.Prompts.text(session, 'Contract: Construction (say "c") or Plant (say "p")?');},
         function (session, results) {session.userData.name = results.response;},
-    function (session, results) {builder.Prompts.text(session, 'Search in: clause numbers (say "n") or index (say "i")?');},
+    function (session) {builder.Prompts.text(session, 'Search in: clause numbers (say "n") or index (say "i")?');},
         function (session, results) {
             session.userData.type = results.response;
             builder.Prompts.text(session, 'Say "search".');}
