1d8d58c5d71b11b8e5a5e331fce66aeec55a207a
Peter Boswell <petergboswell@hotmail.com>
2017-09-11 01:08:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ const scriptRulesIndex = require('./scriptIndex.json');
 const scriptRulesIndexPlant = require('./scriptIndexPlant.json');
 
 var connector = new builder.ChatConnector({
-    //appId: process.env.MY_APP_ID,
-    //appPassword: process.env.MY_APP_PASSWORD
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
     //appId: 'c6076e92-cae8-4b0b-83da-565b83d5c8c6',
-    appId: 'FIDICbot',
-    appPassword: 'Zaw78dhPPxHtqPqFZi6eeEf'
+    //appId: 'FIDICbot',
+    //appPassword: 'Zaw78dhPPxHtqPqFZi6eeEf'
 });
 var bot = new builder.UniversalBot(connector);
 var iFound = 0;
