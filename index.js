c55ac4417c8261bf1aefd16c344fd8be5b17a95a
Peter Boswell <petergboswell@bluewin.ch>
2020-01-21 15:49:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+console.log("..........................in app.js");
+
 const path = require('path');
 const restify = require('restify');
 
