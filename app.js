958cf26fe5c5d5a5dc3d1f072b75027497634322
Peter Boswell <petergboswell@bluewin.ch>
2020-01-21 15:41:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,9 +9,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(bodyParser.json());
 
+console.log("..........................in app.js");
+
 app.get('/', function(req, res) {
     res.render('index', {
-        appToken: process.env.SMOOCH_APP_TOKEN
+        //appToken: process.env.SMOOCH_APP_TOKEN
+        appToken: process.env.MicrosoftAppId,
+        //appPassword: process.env.MicrosoftAppPassword
     });
 });
 
