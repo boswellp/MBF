4659994180f153683b9cee35628a41d200582e1e
Peter Boswell <petergboswell@bluewin.ch>
2020-02-24 01:00:15 +0100

    Add files via upload

---
+++ dialogs/suggestedActionsBot.js
@@ -0,0 +1,67 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const { ActivityHandler, MessageFactory } = require('botbuilder');
+
+class SuggestedActionsBot extends ActivityHandler {
+    constructor() {
+        super();
+
+        this.onMembersAdded(async (context, next) => {
+            await this.sendWelcomeMessage(context);
+
+            // By calling next() you ensure that the next BotHandler is run.
+            await next();
+        });
+
+        this.onMessage(async (context, next) => {
+            const text = context.activity.text;
+
+            // Create an array with the valid color options.
+            const validColors = ['Cons1', 'Plant1', 'EPCT1'];
+
+            // If the `text` is in the Array, a valid color was selected and send agreement.
+            if (validColors.includes(text)) {
+                await context.sendActivity(`${ text } chosen`);
+            } else {
+                await context.sendActivity('Please select a contract.');
+            }
+
+            // After the bot has responded send the suggested actions.
+            await this.sendSuggestedActions(context);
+
+            // By calling next() you ensure that the next BotHandler is run.
+            await next();
+        });
+    }
+
+    /**
+     * Send a welcome message along with suggested actions for the user to click.
+     * @param {TurnContext} turnContext A TurnContext instance containing all the data needed for processing this conversation turn.
+     */
+    async sendWelcomeMessage(turnContext) {
+        const { activity } = turnContext;
+
+        // Iterate over all new members added to the conversation.
+        for (const idx in activity.membersAdded) {
+            if (activity.membersAdded[idx].id !== activity.recipient.id) {
+                const welcomeMessage = `Welcome to FIDICchatbot ${ activity.membersAdded[idx].name }. ` +
+                    'This chatbot allows you to search FIDIC contracts. ' +
+                    'Please select a contract:';
+                await turnContext.sendActivity(welcomeMessage);
+                await this.sendSuggestedActions(turnContext);
+            }
+        }
+    }
+
+    /**
+     * Send suggested actions to the user.
+     * @param {TurnContext} turnContext A TurnContext instance containing all the data needed for processing this conversation turn.
+     */
+    async sendSuggestedActions(turnContext) {
+        var reply = MessageFactory.suggestedActions(['Cons1', 'Plant1', 'EPCT1'], 'Which contract?');
+        await turnContext.sendActivity(reply);
+    }
+}
+
+module.exports.SuggestedActionsBot = SuggestedActionsBot;
