ff86cf3c3c9e57613b08de5253ee2eeb9b33a293
Peter Boswell <petergboswell@bluewin.ch>
2020-02-28 02:05:43 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,59 +1,29 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+// index.js is used to setup and configure your bot
 
-/*//express for viber
-const express = require('express')
-const app = express()
-const port = 3000
-var viber = require('botbuilder-viber')
-var viberOptions = {
-  Token: process.env.VIBER_TOKEN,
-  Name: 'ViberBotName',  
-  AvatarUrl: 'http://url.to/pngfile'
-}
-var viberChannel = new viber.ViberEnabledConnector(viberOptions)
-const winston = require('winston');
-/////////*/
-
+// Import required packages
 const path = require('path');
 const restify = require('restify');
 
+// Import required bot services.
+// See https://aka.ms/bot-services to learn more about the different parts of a bot.
+const { BotFrameworkAdapter, ConversationState, InputHints, MemoryStorage, UserState } = require('botbuilder');
 
+const { FlightBookingRecognizer } = require('./dialogs/flightBookingRecognizer');
 
-const { BotFrameworkAdapter, MemoryStorage, UserState, ConversationState } = require('botbuilder');
-
-const { ActivityTypes } = require('botbuilder-core');
-
-const { QnAMaker } = require('botbuilder-ai');
-
-const { QnAMultiturnBot } = require('./bots/QnAMultiturnBot');
-const { RootDialog } = require('./dialogs/rootDialog');
-
-
-//see sample at https://github.com/BotBuilderCommunity/botbuilder-community-js/tree/master/samples/adapter-twilio-whatsapp
-const { TwilioWhatsAppAdapter } = require('@botbuildercommunity/adapter-twilio-whatsapp');
-
-const { AdaptiveCardsBot } = require('./bots/adaptiveCardsBot');
-
+// This bot's main dialog.
 const { DialogAndWelcomeBot } = require('./bots/dialogAndWelcomeBot');
 const { MainDialog } = require('./dialogs/mainDialog');
 
-const { QnABot } = require('./bots/QnABot');
+// the bot's booking dialog
+const { BookingDialog } = require('./dialogs/bookingDialog');
+const BOOKING_DIALOG = 'bookingDialog';
 
-const { WelcomeBot } = require('./bots/welcomeBot');
-
-// Read environment variables from .env file
+// Note: Ensure you have a .env file and include LuisAppId, LuisAPIKey and LuisAPIHostName.
 const ENV_FILE = path.join(__dirname, '.env');
 require('dotenv').config({ path: ENV_FILE });
-
-// Create HTTP server
-const server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function() {
-    console.log(`\n${ server.name } listening to ${ server.url }`);
-    console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
-    console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
-});
 
 // Create adapter.
 // See https://aka.ms/about-bot-adapter to learn more about adapters.
@@ -62,58 +32,8 @@ const adapter = new BotFrameworkAdapter({
     appPassword: process.env.MicrosoftAppPassword
 });
 
-
-//see https://www.npmjs.com/package/@botbuildercommunity/adapter-twilio-whatsapp
-const whatsAppAdapter = new TwilioWhatsAppAdapter({
-    accountSid: process.env.PROCESS,
-    authToken: process.env.TOKEN,
-    phoneNumber: process.env.PHONE,
-    endpointUrl: process.env.ENDPOINT
-});
-
-// Define state store for your bot.
-// See https://aka.ms/about-bot-state to learn more about bot state.
-const memoryStorage = new MemoryStorage();
-
-// Create user and conversation state with in-memory storage provider.
-const userState = new UserState(memoryStorage);
-const conversationState = new ConversationState(memoryStorage);
-
-
-const qnaService = new QnAMaker({
-    knowledgeBaseId: process.env.QnAKnowledgebaseId,
-    endpointKey: process.env.QnAEndpointKey,
-    host: process.env.QnAEndpointHostName
-});
-
-// Create the main dialog.
-//const dialog = new MainDialog(userState);
-
-// Create the main dialog for multiturn
-//const dialog = new RootDialog(qnaService);
-
-//const bot = new DialogAndWelcomeBot(conversationState, userState, dialog);
-//const bot = new QnABot();
-
-// bots main handler for multiturn
-//const bot = new QnAMultiturnBot(conversationState, userState, dialog);
-
-
-// Create the main dialog for core
-//const bookingDialog = new BookingDialog(BOOKING_DIALOG);
-//const bot = new MainDialog(dialog, userState);
-
-const dialog = new RootDialog(qnaService);
-const bot = new DialogAndWelcomeBot(conversationState, userState, dialog);
-
-
-//const dialog = new DialogAndWelcomeBot(conversationState, userState, dialogQnA);
-// welcomeBot Create the main dialog.
-//const bot = new WelcomeBot(userState, dialog);
-
-
 // Catch-all for errors.
-adapter.onTurnError = async (context, error) => {
+const onTurnErrorHandler = async (context, error) => {
     // This check writes out errors to console log .vs. app insights.
     // NOTE: In production environment, you should consider logging this to Azure
     //       application insights.
@@ -128,31 +48,68 @@ adapter.onTurnError = async (context, error) => {
     );
 
     // Send a message to the user
-    await context.sendActivity('The bot encountered an error or bug.');
-    await context.sendActivity('To continue to run this bot, please fix the bot source code.');
+    let onTurnErrorMessage = 'The bot encountered an error or bug.';
+    await context.sendActivity(onTurnErrorMessage, onTurnErrorMessage, InputHints.ExpectingInput);
+    onTurnErrorMessage = 'To continue to run this bot, please fix the bot source code.';
+    await context.sendActivity(onTurnErrorMessage, onTurnErrorMessage, InputHints.ExpectingInput);
     // Clear out state
-    await conversationState.clear(context);
+    await conversationState.delete(context);
 };
 
-// Listen for incoming requests.
+// Set the onTurnError for the singleton BotFrameworkAdapter.
+adapter.onTurnError = onTurnErrorHandler;
+
+// Define a state store for your bot. See https://aka.ms/about-bot-state to learn more about using MemoryStorage.
+// A bot requires a state store to persist the dialog and user state between messages.
+
+// For local development, in-memory storage is used.
+// CAUTION: The Memory Storage used here is for local bot debugging only. When the bot
+// is restarted, anything stored in memory will be gone.
+const memoryStorage = new MemoryStorage();
+const conversationState = new ConversationState(memoryStorage);
+const userState = new UserState(memoryStorage);
+
+// If configured, pass in the FlightBookingRecognizer.  (Defining it externally allows it to be mocked for tests)
+const { LuisAppId, LuisAPIKey, LuisAPIHostName } = process.env;
+const luisConfig = { applicationId: LuisAppId, endpointKey: LuisAPIKey, endpoint: `https://${ LuisAPIHostName }` };
+
+const luisRecognizer = new FlightBookingRecognizer(luisConfig);
+
+// Create the main dialog.
+const bookingDialog = new BookingDialog(BOOKING_DIALOG);
+const dialog = new MainDialog(luisRecognizer, bookingDialog);
+const bot = new DialogAndWelcomeBot(conversationState, userState, dialog);
+
+// Create HTTP server
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function() {
+    console.log(`\n${ server.name } listening to ${ server.url }`);
+    console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
+    console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
+});
+
+// Listen for incoming activities and route them to your bot main dialog.
 server.post('/api/messages', (req, res) => {
-    adapter.processActivity(req, res, async (context) => {
-        // Route to main dialog.
-        await bot.run(context);
+    // Route received a request to adapter for processing
+    adapter.processActivity(req, res, async (turnContext) => {
+        // route to bot activity handler.
+        await bot.run(turnContext);
     });
 });
 
-/*
-//see https://github.com/DreamTeamMobile/botbuilder-viber
-bot.connector(viber.ViberChannelId, viberChannel)
-app.use('/viber/webhook', viberChannel.listen())
-*/
+// Listen for Upgrade requests for Streaming.
+server.on('upgrade', (req, socket, head) => {
+    // Create an adapter scoped to this WebSocket connection to allow storing session data.
+    const streamingAdapter = new BotFrameworkAdapter({
+        appId: process.env.MicrosoftAppId,
+        appPassword: process.env.MicrosoftAppPassword
+    });
+    // Set onTurnError for the BotFrameworkAdapter created for each connection.
+    streamingAdapter.onTurnError = onTurnErrorHandler;
 
-// WhatsApp endpoint for Twilio
-//see https://www.npmjs.com/package/@botbuildercommunity/adapter-twilio-whatsapp
-server.post('/api/whatsapp/messages', (req, res) => {
-    whatsAppAdapter.processActivity(req, res, async (context) => {
-        // Route to main dialog.
+    streamingAdapter.useWebSocket(req, socket, head, async (context) => {
+        // After connecting via WebSocket, run this logic for every request sent over
+        // the WebSocket connection.
         await bot.run(context);
     });
 });
---
+++ index_WORKS.js
@@ -0,0 +1,161 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+
+/*//express for viber
+const express = require('express')
+const app = express()
+const port = 3000
+var viber = require('botbuilder-viber')
+var viberOptions = {
+  Token: process.env.VIBER_TOKEN,
+  Name: 'ViberBotName',  
+  AvatarUrl: 'http://url.to/pngfile'
+}
+var viberChannel = new viber.ViberEnabledConnector(viberOptions)
+const winston = require('winston');
+/////////*/
+
+const path = require('path');
+const restify = require('restify');
+
+
+
+const { BotFrameworkAdapter, MemoryStorage, UserState, ConversationState } = require('botbuilder');
+
+const { ActivityTypes } = require('botbuilder-core');
+
+const { QnAMaker } = require('botbuilder-ai');
+
+const { QnAMultiturnBot } = require('./bots/QnAMultiturnBot');
+const { RootDialog } = require('./dialogs/rootDialog');
+
+
+//see sample at https://github.com/BotBuilderCommunity/botbuilder-community-js/tree/master/samples/adapter-twilio-whatsapp
+const { TwilioWhatsAppAdapter } = require('@botbuildercommunity/adapter-twilio-whatsapp');
+
+const { AdaptiveCardsBot } = require('./bots/adaptiveCardsBot');
+
+const { DialogAndWelcomeBot } = require('./bots/dialogAndWelcomeBot');
+const { MainDialog } = require('./dialogs/mainDialog');
+
+const { QnABot } = require('./bots/QnABot');
+
+const { WelcomeBot } = require('./bots/welcomeBot');
+
+// Read environment variables from .env file
+const ENV_FILE = path.join(__dirname, '.env');
+require('dotenv').config({ path: ENV_FILE });
+
+// Create HTTP server
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function() {
+    console.log(`\n${ server.name } listening to ${ server.url }`);
+    console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
+    console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
+});
+
+// Create adapter.
+// See https://aka.ms/about-bot-adapter to learn more about adapters.
+const adapter = new BotFrameworkAdapter({
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword
+});
+
+
+//see https://www.npmjs.com/package/@botbuildercommunity/adapter-twilio-whatsapp
+const whatsAppAdapter = new TwilioWhatsAppAdapter({
+    accountSid: 'ACf0be7022495c01ad04e8899935ddb59e', // Account SID
+    authToken: '1034e741f296daf7cf0f01d7eacea17b', // Auth Token
+    phoneNumber: 'whatsapp:+14155238886',// The From parameter consisting of whatsapp: followed by the sending WhatsApp number (using E.164 formatting
+    endpointUrl: 'https://fidicchatbot.herokuapp.com/api/whatsapp/messages' // Endpoint URL you configured in the sandbox, used for validation
+});
+
+
+
+// Define state store for your bot.
+// See https://aka.ms/about-bot-state to learn more about bot state.
+const memoryStorage = new MemoryStorage();
+
+// Create user and conversation state with in-memory storage provider.
+const userState = new UserState(memoryStorage);
+const conversationState = new ConversationState(memoryStorage);
+
+
+const qnaService = new QnAMaker({
+    knowledgeBaseId: process.env.QnAKnowledgebaseId,
+    endpointKey: process.env.QnAEndpointKey,
+    host: process.env.QnAEndpointHostName
+});
+
+// Create the main dialog.
+//const dialog = new MainDialog(userState);
+
+// Create the main dialog for multiturn
+//const dialog = new RootDialog(qnaService);
+
+//const bot = new DialogAndWelcomeBot(conversationState, userState, dialog);
+//const bot = new QnABot();
+
+// bots main handler for multiturn
+//const bot = new QnAMultiturnBot(conversationState, userState, dialog);
+
+
+// Create the main dialog for core
+//const bookingDialog = new BookingDialog(BOOKING_DIALOG);
+//const bot = new MainDialog(dialog, userState);
+
+const dialog = new RootDialog(qnaService);
+const bot = new DialogAndWelcomeBot(conversationState, userState, dialog);
+
+
+//const dialog = new DialogAndWelcomeBot(conversationState, userState, dialogQnA);
+// welcomeBot Create the main dialog.
+//const bot = new WelcomeBot(userState, dialog);
+
+
+// Catch-all for errors.
+adapter.onTurnError = async (context, error) => {
+    // This check writes out errors to console log .vs. app insights.
+    // NOTE: In production environment, you should consider logging this to Azure
+    //       application insights.
+    console.error(`\n [onTurnError] unhandled error: ${ error }`);
+
+    // Send a trace activity, which will be displayed in Bot Framework Emulator
+    await context.sendTraceActivity(
+        'OnTurnError Trace',
+        `${ error }`,
+        'https://www.botframework.com/schemas/error',
+        'TurnError'
+    );
+
+    // Send a message to the user
+    await context.sendActivity('The bot encountered an error or bug.');
+    await context.sendActivity('To continue to run this bot, please fix the bot source code.');
+    // Clear out state
+    await conversationState.clear(context);
+};
+
+// Listen for incoming requests.
+server.post('/api/messages', (req, res) => {
+    adapter.processActivity(req, res, async (context) => {
+        // Route to main dialog.
+        await bot.run(context);
+    });
+});
+
+/*
+//see https://github.com/DreamTeamMobile/botbuilder-viber
+bot.connector(viber.ViberChannelId, viberChannel)
+app.use('/viber/webhook', viberChannel.listen())
+*/
+
+// WhatsApp endpoint for Twilio
+//see https://www.npmjs.com/package/@botbuildercommunity/adapter-twilio-whatsapp
+server.post('/api/whatsapp/messages', (req, res) => {
+    whatsAppAdapter.processActivity(req, res, async (context) => {
+        // Route to main dialog.
+        await bot.run(context);
+    });
+});
+
