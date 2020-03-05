b1a9cb1b26ba8151e5b9fc6db73609b901197f0b
Peter Boswell <petergboswell@bluewin.ch>
2020-03-05 23:14:09 +0100

    Update dialogAndWelcomeBot.js

--- bots/dialogAndWelcomeBot.js
+++ bots/dialogAndWelcomeBot.js
@@ -3,15 +3,80 @@
 
 const { DialogBot } = require('./dialogBot');
 
+
 const { CardFactory } = require('botbuilder');
+const { ActivityHandler } = require('botbuilder');
 
 const welcomeCard = require('../resources/WelcomeCard.json');
 
-class DialogAndWelcomeBot extends DialogBot {
+const CONVERSATION_DATA_PROPERTY = 'conversationData';
+const USER_PROFILE_PROPERTY = 'userProfile';
+
+class DialogAndWelcomeBot extends DialogBot { //used this for multiturn
+
+//class DialogAndWelcomeBot extends ActivityHandler { //shows name, etc
 
     constructor(conversationState, userState, dialog) {
 
         super(conversationState, userState, dialog);
+
+///////
+        this.conversationDataAccessor = conversationState.createProperty(CONVERSATION_DATA_PROPERTY);
+        this.userProfileAccessor = userState.createProperty(USER_PROFILE_PROPERTY);
+        this.conversationState = conversationState;
+        this.userState = userState;
+///////
+
+
+       this.onMessage(async (turnContext, next) => {
+            // Get the state properties from the turn context.
+            const userProfile = await this.userProfileAccessor.get(turnContext, {});
+            const conversationData = await this.conversationDataAccessor.get(
+                turnContext, { promptedForUserName: false });
+
+            if (!userProfile.name) {
+                // First time around this is undefined, so we will prompt user for name.
+                if (conversationData.promptedForUserName) {
+                    // Set the name to what the user provided.
+                    userProfile.name = turnContext.activity.text;
+
+                    //await turnContext.sendActivity(`Thanks2222 ${ userProfile.name }. To see conversation data, type anything.`);
+                    await turnContext.sendActivity('');
+
+
+                    // Reset the flag to allow the bot to go though the cycle again.
+                    conversationData.promptedForUserName = false;
+
+                    await next();//MINE ADDED
+                } 
+                else 
+                {
+                    //await turnContext.sendActivity('1. What is your name?');
+                    await turnContext.sendActivity('');
+                    // Set the flag to true, so we don't prompt in the next turn.
+                    conversationData.promptedForUserName = true;
+                }
+            } 
+            else
+            {
+                // Add message details to the conversation data.
+                conversationData.timestamp = turnContext.activity.timestamp.toLocaleString();
+                conversationData.channelId = turnContext.activity.channelId;
+
+                // Display state data.
+                //await turnContext.sendActivity(`${ userProfile.name } sent3333: ${ turnContext.activity.text }`);
+                //await turnContext.sendActivity(`Message received at: ${ conversationData.timestamp }`);
+                //await turnContext.sendActivity(`Message received from: ${ conversationData.channelId }`);
+            }
+
+            await next();
+        });
+
+
+
+
+///////
+
 
         this.onMembersAdded(async (context, next) => {
 
@@ -37,7 +102,11 @@ class DialogAndWelcomeBot extends DialogBot {
 
         welcomeCard.body[3].text = 'Submit "start" or "help" anytime to start again and for help.';
 
-        welcomeCard.body[4].text = 'Submit "start" to start.';
+        welcomeCard.body[4].text = 'After selecting a contract, you can display clauses by submiting a clause number or a keyword (keywords search in the index of clauses).';
+
+        welcomeCard.body[5].text = 'Shortcut codes for contracts that can be submitted at any time are:\n- "c1" - Construction Contract 1st Ed 1999\n- "p1" - Plant Contract 1st Ed 1999.';
+
+        welcomeCard.body[5].text = 'The FIDICchatbot is developed by Bricad Associates, Switzerland, as part of the https://FIDIC.tips initiative. It complements the FIDICbot that offers similar functionality and allows full-text searching.';
 
         welcomeCard.actions[0].title = 'Privacy policy';
 
@@ -52,17 +121,27 @@ class DialogAndWelcomeBot extends DialogBot {
 //////////////////////
 
 
-
-
-                    //await context.sendActivity(card);
-
                 }
             }
-
-            // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
     }
+
+/////////
+    /**
+     * Override the ActivityHandler.run() method to save state changes after the bot logic completes.
+     */
+
+     async run(context) {
+        await super.run(context);
+
+        // Save any state changes. The load happened during the execution of the Dialog.
+        await this.conversationState.saveChanges(context, false);
+        await this.userState.saveChanges(context, false);
+        //console.log (".............dialog and welcome SAVE context = " + JSON.stringify(context));
+    }
+
+//////////
 
 
 }
