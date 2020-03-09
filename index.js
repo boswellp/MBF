14debdbd110a762c4a0a7ce513223e0cb459da78
Peter Boswell <petergboswell@bluewin.ch>
2020-03-09 21:33:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,24 @@
-// Copyright (c) Bricad Associates, 9 March 2020
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
-/*//express for viber
-const express = require('express')
-const app = express()
-const port = 3000
-var viber = require('botbuilder-viber')
-var viberOptions = {
-  Token: process.env.VIBER_TOKEN,
-  Name: 'ViberBotName',  
-  AvatarUrl: 'http://url.to/pngfile'
-}
-var viberChannel = new viber.ViberEnabledConnector(viberOptions)
-const winston = require('winston');
-/////////*/
+// index.js is used to setup and configure your bot
 
+// Import required packages
 const path = require('path');
 const restify = require('restify');
 
+// Import required bot services. See https://aka.ms/bot-services to learn more about the different parts of a bot.
 const { BotFrameworkAdapter, ConversationState, MemoryStorage, UserState } = require('botbuilder');
-
-const { ActivityTypes } = require('botbuilder-core');
-
 const { QnAMaker } = require('botbuilder-ai');
 
 const { QnAMultiturnBot } = require('./bots/QnAMultiturnBot');
-const { DialogAndWelcomeBot } = require('./bots/dialogAndWelcomeBot');
-
 const { RootDialog } = require('./dialogs/rootDialog');
 
-
+// Note: Ensure you have a .env file and include QnAMakerKnowledgeBaseId, QnAMakerEndpointKey and QnAMakerHost.
 const ENV_FILE = path.join(__dirname, '.env');
 require('dotenv').config({ path: ENV_FILE });
 
-
+// Create HTTP server.
 const server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3979, function() {
     console.log(`\n${ server.name } listening to ${ server.url }.`);
@@ -40,14 +26,21 @@ server.listen(process.env.port || process.env.PORT || 3979, function() {
     console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
 });
 
-
+// Create adapter.
+// See https://aka.ms/about-bot-adapter to learn more about adapters.
 const adapter = new BotFrameworkAdapter({
     appId: process.env.MicrosoftAppId,
     appPassword: process.env.MicrosoftAppPassword
 });
 
+// Catch-all for errors.
 adapter.onTurnError = async (context, error) => {
+    // This check writes out errors to console log .vs. app insights.
+    // NOTE: In production environment, you should consider logging this to Azure
+    //       application insights.
     console.error(`\n [onTurnError] unhandled error: ${ error }`);
+
+    // Send a trace activity, which will be displayed in Bot Framework Emulator
     await context.sendTraceActivity(
         'OnTurnError Trace',
         `${ error }`,
@@ -55,12 +48,16 @@ adapter.onTurnError = async (context, error) => {
         'TurnError'
     );
 
+    // Send a message to the user
     await context.sendActivity('The bot encountered an error or bug.');
     await context.sendActivity('To continue to run this bot, please fix the bot source code.');
 };
 
+// Define the state store for your bot. See https://aka.ms/about-bot-state to learn more about using MemoryStorage.
+// A bot requires a state storage system to persist the dialog and user state between messages.
 const memoryStorage = new MemoryStorage();
 
+// Create conversation and user state with in-memory storage provider.
 const conversationState = new ConversationState(memoryStorage);
 const userState = new UserState(memoryStorage);
 
@@ -79,11 +76,13 @@ const qnaService = new QnAMaker({
     host: endpointHostName
 });
 
+// Create the main dialog.
+const dialog = new RootDialog(qnaService);
 
-//const dialog = new RootDialog(qnaService);  //ORIG
-const dialog = new RootDialog(qnaService, userState);  //MINE
+// Create the bot's main handler.
 const bot = new QnAMultiturnBot(conversationState, userState, dialog);
 
+// Listen for incoming requests.
 server.post('/api/messages', (req, res) => {
     adapter.processActivity(req, res, async (turnContext) => {
         // Route the message to the bot's main handler.
