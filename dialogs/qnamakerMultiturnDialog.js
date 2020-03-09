a9f4fb9896a021ebdce7b259ca07bd97dafb3319
Peter Boswell <petergboswell@bluewin.ch>
2020-03-09 22:39:23 +0100

    Update qnamakerMultiturnDialog.js

--- dialogs/qnamakerMultiturnDialog.js
+++ dialogs/qnamakerMultiturnDialog.js
@@ -75,6 +75,243 @@ class QnAMakerMultiturnDialog extends ComponentDialog {
             qnaMakerOptions.scoreThreshold = qnaMakerOptions.scoreThreshold ? qnaMakerOptions.scoreThreshold : DefaultThreshold;
             qnaMakerOptions.top = qnaMakerOptions.top ? qnaMakerOptions.top : DefaultThreshold;
         }
+        
+        //////////////////////////////////////////MINE
+        
+                console.log("\n\n78 ..........MULTITURN..............");
+
+        var util = require('util')
+        //console.log("\n85 MULTITURN this._userProfileAccessor  = " + util.inspect(this._userProfileAccessor))
+        //console.log("\n86 MULTITURN this._userContractAccessor  = " + util.inspect(this._userContractAccessor))
+
+        var JSONstringifythisuserState = JSON.stringify(this._userState);
+        //console.log ("\n89 MULTITURN this._userState = " + JSONstringifythisuserState);
+
+ 
+        if (this._userProfileAccessor.profileName == 'undefined' && JSONstringifythisuserState.indexOf('cons1',0) != -1) //cons1 comes from QnAMaker and not got profileName
+             {
+             this._userProfileAccessor.profileName = "c1";
+             console.log ("\n90 MULTITURN cons1 from QnAMaker IN c1");
+             //var utilProfileAccessor = util.inspect(this._userProfileAccessor);
+             //console.log("96 utiluserProfileAccessor  = " + utilProfileAccessor )
+             } 
+             else if (JSONstringifythisuserState.indexOf('plant1',0) != -1){ //plant1 comes from QnAMaker
+             this._userProfileAccessor.profileName = "p1";
+             console.log ("\n95 MULTITURN plant1 from QnAMake IN p1");
+             } 
+
+
+        var gotContract = '';
+        gotContract = this._userProfileAccessor.profileName;
+
+        console.log("\n103 MULTITURN profileName  = " + this._userProfileAccessor.profileName)
+
+        var str = stepContext.context.activity.text;
+
+        console.log ("\n108 str = " + str + '; gotContract = ' + gotContract +'\n.................................................');
+     
+        if (str != 'start')
+             {
+             console.log ("\n112 IN str = " + str);
+
+             if ((gotContract != '' && str == 'c1i') || (gotContract != '' && str == 'c1 i'))  //INDEX c1 i button or c1i typed in
+                 {
+                 console.log ("\n113 button c1 i or typed c1i str = " + str + '\n');
+                 stepContext.context.activity.text = 'c1i:0/0/0/0';
+                 this._userProfileAccessor.profileName = 'c1i';
+                 }
+
+
+                 else if ((gotContract != '' && str == 'p1i') || (gotContract != '' && str == 'p1 i'))  //INDEX c1 i button or c1i typed in
+                 {
+                 console.log ("\n121 button p1 i or typed p1i str = " + str + '\n');
+                 stepContext.context.activity.text = 'p1i:0/0/0/0';
+                 this._userProfileAccessor.profileName = 'p1i';
+                 }
+
+                 else if (gotContract == 'c1' && str.indexOf('Plant & Design-Build Contract 1st Ed 1999',0) != -1)
+                 {
+                 console.log ("\n128 got c1 str = " + str + '\n');
+                 stepContext.context.activity.text = 'Plant & Design-Build Contract 1st Ed 1999';
+                 this._userProfileAccessor.profileName = 'p1'; //change contract after restart
+                 }
+
+                 else if (gotContract == 'p1' && str.indexOf('Construction Contract 1st Ed 1999',0) != -1)
+                 {
+                 console.log ("\n135 got p1 str = " + str + '\n');
+                 stepContext.context.activity.text = 'Construction Contract 1st Ed 1999';
+                 this._userProfileAccessor.profileName = 'c1'; //change contract after restart
+                 } 
+                       
+                 else
+
+                 {
+                 
+                 var strClean = str; 
+                 console.log ("\n148 IN strClean = " + strClean);  
+
+                 if (isNaN(str)) //keyword
+                      {
+                      var contractCode = this._userProfileAccessor.profileName;
+                      console.log("\n153 keyword str = " + strClean + '; contractCode = ' + contractCode);
+ 
+                      ////Enter contract on clicking start
+
+                      if (str.indexOf('Construction Contract',0) != -1)
+                          {
+                          stepContext.context.activity.text = 'cons1'; //cons1 into userProfile.name
+                          this._userProfileAccessor.profileName = 'c1'
+                          console.log ("155 SENT stepContext.context.activity.text = " + stepContext.context.activity.text);
+                          }
+                          else if (str.indexOf('Plant &',0) != -1 )
+                          {
+                          stepContext.context.activity.text = 'plant1'; //cons1 into userProfile.name
+                          this._userProfileAccessor.profileName = 'p1'
+                          console.log ("167 SENT stepContext.context.activity.text = " + stepContext.context.activity.text);
+                          }
+                      //}
+
+                     
+                      else if (str == 'c1i' || str == 'p1i')//INDEX
+                              {
+                              if (str != this._userProfileAccessor.profileName) //changing index for c1i to p1i
+                                  {
+                                  this._userProfileAccessor.profileName = str + ':0/0/0/0';
+                                  stepContext.context.activity.text = str;
+                                  console.log ("181 SENT INDEX stepContext.context.activity.text = " + stepContext.context.activity.text);
+                                  }
+                              }
+                      else if ((contractCode.indexOf('1i',0) != -1 && str == 'c1') || (contractCode.indexOf('1i',0) != -1 && str == 'p1')) //leave index
+                              {  
+                              stepContext.context.activity.text = str;
+                              this._userProfileAccessor.profileName = str; //change index to c1, p1
+                              console.log ("188 SENT INDEX stepContext.context.activity.text = " + stepContext.context.activity.text);
+                              }
+                       else if (contractCode.indexOf('1i',0) != -1) //got contact code + str
+                              {
+                              stepContext.context.activity.text = contractCode + ':' + str;
+                              console.log ("193 SENT INDEX stepContext.context.activity.text = " + stepContext.context.activity.text);
+                              }
+
+
+                       else
+                             {
+                             console.log("\n196 keyword strClean = " + strClean + '; contractCode = ' + contractCode);
+                             var posnSpace = strClean.indexOf(' ',0);
+                             console.log ("198 posnSpace =" + posnSpace);
+
+                             var strCon = str;
+                             if (posnSpace != -1){strCon = str.substring(0,posnSpace);}
+                             console.log ("202 strCon =" + strCon);
+
+                             if (strCon == "c1" || strCon == "p1" )
+                                 {
+                                 this._userProfileAccessor.profileName = strCon;
+                                 console.log("\n219 profileName = " + this._userProfileAccessor.profileName)
+                                 strCon = strCon + ":";
+                                 console.log ("221 strCon =" + strCon);
+                                 }
+
+                             var strNo ='';
+                             if (posnSpace != -1)  //one word
+                                 {
+                                 var strNo = strClean.substring(posnSpace+1,strClean.length);
+                                 }
+                                console.log ("226 strNo =" + strNo);
+
+                             var strNoFull = strNo;
+                             if (posnSpace != -1)
+                                 {
+                                 if (strNo.length == 1){strNoFull = strNoFull+'.0.0.0';}
+                                 if (strNo.length == 3){strNoFull = strNoFull+'.0.0';}
+                                 if (strNo.length == 5){strNoFull = strNoFull+'.0.0';}
+                                 }
+                             console.log ("226 strNoFull =" + strNoFull);
+
+                             if (str =='c1' || str == 'p1')
+                                 {
+                                 strConNoFull = str;
+                                 }
+                                 else if (strNoFull != '' && (strCon == "c1" || strCon == "p1") )
+                                    {
+                                    var strConNoFull = strCon + " " + strNoFull;
+                                    }
+                                 else
+                                    {
+                                    var strConNoFull = strCon + strNoFull;
+                                    }
+
+                                    console.log("241 strConNoFull = " + strConNoFull);
+
+                                    strConNoFull = strConNoFull.replace('.','\/');
+                                    strConNoFull = strConNoFull.replace('.','\/');
+                                    strConNoFull = strConNoFull.replace('.','\/');
+                                    strConNoFull = strConNoFull.replace('.','\/');
+                                    strConNoFull = strConNoFull.replace('c1 ','c1:');
+                                    strConNoFull = strConNoFull.replace('p1 ','p1:');
+
+                                    if (strNo == '' && strConNoFull != 'c1' && strConNoFull != 'p1' && strConNoFull != 'c1i' && strConNoFull != 'p1i' && strConNoFull != 'help' && strConNoFull != 'start') 
+                                        {
+                                        strConNoFull = contractCode.replace('1','1i:' + strConNoFull)
+                                        }
+
+                                    console.log("255 A keyword strConNoFull = " + strConNoFull);
+
+                                    stepContext.context.activity.text = strConNoFull;
+
+                                    console.log ("259 SENT stepContext.context.activity.text = " + stepContext.context.activity.text);
+                                     }
+                               }
+                          }
+                     }
+  
+                console.log ("268 str = " + str);
+                    
+
+                 if (str != 'start' && !isNaN(str)) ///number
+                       { 
+                       console.log("\n273 A clause number str = " + str);              
+
+                       var strCon = '';
+
+
+                       if (this._userProfileAccessor.profileName == "c1" || this._userProfileAccessor.profileName == "p1" )
+                             {
+                             console.log("\n282 profileName = " + this._userProfileAccessor.profileName)
+                             strCon = this._userProfileAccessor.profileName + ":";
+                             console.log ("284 strCon =" + strCon);
+                             }
+
+
+                       var strNo = str.toString();
+                       console.log ("289 strNo =" + strNo);
+
+                       var strNoFull = strNo;
+                       if (strNo.length == 1){strNoFull = strNoFull+'.0.0.0';}
+                       if (strNo.length == 3){strNoFull = strNoFull+'.0.0';}
+                       if (strNo.length == 5){strNoFull = strNoFull+'.0.0';}
+                       console.log ("295 strNoFull =" + strNoFull);
+
+                       var strConNoFull = strCon + strNoFull;
+
+                       console.log("299 strConNoFull = " + strConNoFull);
+
+                       strConNoFull = strConNoFull.replace('.','\/');
+                       strConNoFull = strConNoFull.replace('.','\/');
+                       strConNoFull = strConNoFull.replace('.','\/');
+                       strConNoFull = strConNoFull.replace('.','\/');
+                       strConNoFull = strConNoFull.replace('c1 ','c1:');
+                       strConNoFull = strConNoFull.replace('p1 ','p1:');
+
+                       console.log("308 A keyword strConNoFull = " + strConNoFull);
+
+                       stepContext.context.activity.text = strConNoFull;
+
+                       console.log ("312 SENT stepContext.context.activity.text = " + stepContext.context.activity.text);
+
+                       }
+        
+        //////////////////////////////////////////END MINE
 
         // Storing the context info
         stepContext.values[CurrentQuery] = stepContext.context.activity.text;
