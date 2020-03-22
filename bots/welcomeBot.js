51be608b3032005335556c331ed774a41b70934f
Peter Boswell <petergboswell@bluewin.ch>
2020-03-23 00:36:59 +0100

    Add files via upload

--- bots/welcomeBot.js
+++ bots/welcomeBot.js
@@ -16,25 +16,21 @@ class WelcomeBot extends ActivityHandler {
         this.welcomedUserProperty = userState.createProperty(WELCOMED_USER);
 
         this.userState = userState;
-/*
+
+
         this.onMessage(async (context, next) => {
-            // Read UserState. If the 'DidBotWelcomedUser' does not exist (first time ever for a user)
-            // set the default to false.
+
             const didBotWelcomedUser = await this.welcomedUserProperty.get(context, false);
 
-            // Your bot should proactively send a welcome message to a personal chat the first time
-            // (and only the first time) a user initiates a personal chat with your bot.
+
             if (didBotWelcomedUser === false) {
-                // The channel should send the user name in the 'From' object
+
                 const userName = context.activity.from.name;
                 await context.sendActivity('You are seeing this message because this was your first message ever sent to this bot.');
                 await context.sendActivity(`It is a good practice to welcome the user and provide personal greeting. For example, welcome ${ userName }.`);
 
-                // Set the flag indicating the bot handled the user's first message.
                 await this.welcomedUserProperty.set(context, true);
             } else {
-                // This example uses an exact match on user's input utterance.
-                // Consider using LUIS or QnA for Natural Language Processing.
                 const text = context.activity.text.toLowerCase();
                 switch (text) {
                 case 'hello':
@@ -46,20 +42,18 @@ class WelcomeBot extends ActivityHandler {
                     await this.sendIntroCard(context);
                     break;
                 default:
-                    await context.sendActivity(`This is a simple Welcome Bot sample. You can say 'intro' to
-                                                    see the introduction card. If you are running this bot in the Bot
-                                                    Framework Emulator, press the 'Start Over' button to simulate user joining a bot or a channel`);
+                    await context.sendActivity(`This is a simple Welcome Bot sample. You can say 'intro' to see the introduction card. If you are running this bot in the Bot Framework Emulator, press the 'Start Over' button to simulate user joining a bot or a channel`);
                 }
             }
 
-            // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
-*/        
-        /////////////////////ADDED
-        
-        /////added from https://github.com/microsoft/BotFramework-WebChat/issues/2120#issuecomment-516056614
 
+
+
+/////added from https://github.com/microsoft/BotFramework-WebChat/issues/2120#issuecomment-516056614
+
+//WORKS LOCAL
 
 this.onEvent(async (context, next) => {
   if (context.activity.name === 'webchat/join') {
@@ -68,7 +62,7 @@ this.onEvent(async (context, next) => {
   await next();
 });
 
-        /*
+/**
 this.onMembersAdded(async (context, next) => {
   const { channelId, membersAdded } = context.activity;
 
@@ -81,24 +75,18 @@ this.onMembersAdded(async (context, next) => {
   }
   await next();
 });
-
 */
-        
-  
-       
-        ////////
 
-        // Sends welcome messages to conversation members when they join the conversation.
-        // Messages are only sent to conversation members who aren't the bot.
+
+///////added from end
+
+
+////added end
+
         this.onMembersAdded(async (context, next) => {
-            // Iterate over all new members added to the conversation
             for (const idx in context.activity.membersAdded) {
-                // Greet anyone that was not the target (recipient) of this message.
-                // Since the bot is the recipient for events from the channel,
-                // context.activity.membersAdded === context.activity.recipient.Id indicates the
-                // bot was added to the conversation, and the opposite indicates this is a user.
                 if (context.activity.membersAdded[idx].id !== context.activity.recipient.id) {
-                    await context.sendActivity('Welcome to the \'Welcome User\' Bot. This bot will introduce you to welcoming and greeting users.');
+                    await context.sendActivity('onMembersAdded LONG Welcome to the \'Welcome User\' FIDICchatbot. This bot will introduce you to welcoming and greeting users.');
                     await context.sendActivity("You are seeing this message because the bot received at least one 'ConversationUpdate' " +
                         'event, indicating you (and possibly others) joined the conversation. If you are using the emulator, ' +
                         'pressing the \'Start Over\' button to trigger this event again. The specifics of the \'ConversationUpdate\' ' +
@@ -112,8 +100,9 @@ this.onMembersAdded(async (context, next) => {
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
-        
-        
+
+
+
     }
 
     /**
