7966907ded3f65467554ba4f851459a3db361a15
Peter Boswell <petergboswell@bluewin.ch>
2020-03-23 22:46:56 +0100

    Add files via upload

--- bots/welcomeBot.js
+++ bots/welcomeBot.js
@@ -1,5 +1,5 @@
 // Import required Bot Framework classes.
-const { ActionTypes, ActivityHandler, CardFactory } = require('botbuilder');
+const { ActionTypes, ActivityHandler, CardFactory, MessageFactory } = require('botbuilder');
 
 // Welcomed User property name
 const WELCOMED_USER = 'welcomedUserProperty';
@@ -10,127 +10,127 @@ class WelcomeBot extends ActivityHandler {
      * @param {UserState} User state to persist boolean flag to indicate
      *                    if the bot had already welcomed the user
      */
-    constructor(userState) {
+    constructor(conversationState, userState, dialog) {
         super();
+
+
+        if (!conversationState) throw new Error('[welcomeBot]: Missing parameter. conversationState is required');
+        if (!userState) throw new Error('[welcomeBot]: Missing parameter. userState is required');
+        if (!dialog) throw new Error('[welcomeBot]: Missing parameter. dialog is required');
+
+        this.conversationState = conversationState;
+        this.userState = userState;
+        this.dialog = dialog;
+        this.dialogState = this.conversationState.createProperty('DialogState');
 
         this.welcomedUserProperty = userState.createProperty(WELCOMED_USER);
 
-        this.userState = userState;
-
-                 
-//webchat start
-//method 2
-/*
-this.onEvent(async (context, next) => {
-  if (context.activity.name === 'webchat/join') {
-    await context.sendActivity('Back Channel Welcome Message!');
-    //await context.sendActivity(`Got event, your language is `);
-  }
-  await next();
-});
-
-
-this.onMembersAdded(async (context, next) => {
-  const { channelId, membersAdded } = context.activity;
-
-  if (channelId !== 'directline' && channelId !== 'webchat') {
-    for (let member of membersAdded) {
-      if (member.id !== context.activity.recipient.id) {
-        await context.sendActivity("WORS LOCAL Welcome Message from `onMembersAdded` handler!");
-      }
-    }
-  }
-  await next();
-});
-        
 
         this.onMessage(async (context, next) => {
 
+            // If the 'DidBotWelcomedUser' does not exist (first time ever for a user)
+            // set the default to false.
+
             const didBotWelcomedUser = await this.welcomedUserProperty.get(context, false);
 
+            // Your bot should proactively send a welcome message to a personal chat the first time
+            // (and only the first time) a user initiates a personal chat with your bot.
 
             if (didBotWelcomedUser === false) {
 
-                const userName = context.activity.from.name;
-                await context.sendActivity('You are seeing this message because this was your first message ever sent to this bot.');
-                await context.sendActivity(`It is a good practice to welcome the user and provide personal greeting. For example, welcome ${ userName }.`);
+                await context.sendActivity('FALSE Thank you for making use of FIDICbot. Please let us know if you have any comments. You are seeing this message because this was your first message ever sent to this bot. It is a good practice to welcome the user and provide personal greeting.');
+
 
                 await this.welcomedUserProperty.set(context, true);
-            } else {
-                const text = context.activity.text.toLowerCase();
-                switch (text) {
-                case 'hello':
-                case 'hi':
-                    await context.sendActivity(`You said "${ context.activity.text }"`);
-                    break;
-                case 'intro':
-                case 'help':
-                    await this.sendIntroCard(context);
-                    break;
-                default:
-                    await context.sendActivity(`This is a simple Welcome Bot sample. You can say 'intro' to see the introduction card. If you are running this bot in the Bot Framework Emulator, press the 'Start Over' button to simulate user joining a bot or a channel`);
-                }
+                } 
+
+                else 
+
+                {
+                
+
+
+
             }
 
             await next();
         });
 
-      
-  this.onMembersAdded(async (context, next) => {
-  const { membersAdded } = context.activity;
 
-  for (let member of membersAdded) {
-    if (member.id !== context.activity.recipient.id) {
-      await context.sendActivity("WORKS TO SOME EXTENTWelcome Message from `onMembersAdded` handler!");
-    }
-  }
-  await next();
-});
-              
-  this.onMembersAdded(async (context, next) => {
-  if (context.activity.name === 'webchat/join') {
-    await context.sendActivity('Back Channel Welcome Message!');
-    //await context.sendActivity(`Got event, your language is `);
-  }
-  await next();
-});      
-
-*/
+        // Sends welcome messages to conversation members when they join the conversation. Messages are only sent to conversation members who aren't the bot.
         this.onMembersAdded(async (context, next) => {
-            console.log("100 context.activity = " + context.activity)
-            if (context.activity.name === 'webchat/join') {
-            //for (const idx in context.activity.membersAdded) {
-                //if (context.activity.membersAdded[idx].id !== context.activity.recipient.id) {
-                    await context.sendActivity('onMembersAdded LONG Welcome to the \'Welcome User\' FIDICchatbot. This bot will introduce you to welcoming and greeting users.');
-                    await context.sendActivity("You are seeing this message because the bot received at least one 'ConversationUpdate' " +
-                        'event, indicating you (and possibly others) joined the conversation. If you are using the emulator, ' +
-                        'pressing the \'Start Over\' button to trigger this event again. The specifics of the \'ConversationUpdate\' ' +
-                        'event depends on the channel. You can read more information at https://aka.ms/about-botframework-welcome-user');
-                    await context.sendActivity('It is a good pattern to use this event to send general greeting to user, explaining what your bot can do. ' +
-                        'In this example, the bot handles \'hello\', \'hi\', \'help\' and \'intro\'. ' +
-                        'Try it now, type \'hi\'');
-               // }
+            for (const idx in context.activity.membersAdded) {
+
+                if (context.activity.membersAdded[idx].id !== context.activity.recipient.id) 
+
+                    {
+
+                    await context.sendActivity('Welcome to FIDICchatbot. CARD? This bot will introduce you to welcoming and greeting users.');
+                    await context.sendActivity('It is a good pattern to use this event to send general greeting to user, explaining what your bot can do. In this example, the bot handles \'start\', \'hello\', \'hi\', \'help\' and \'intro\'. ' + 'Try it now, type \'hi\'');
+
+                    await this.sendIntroCard(context);
+                
+
+
+
+                    var reply = MessageFactory.suggestedActions(['start'], 'Please submit "start" to start.');
+                    await context.sendActivity(reply);
+
+
+                    }
             }
 
-            // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
 
 
+        this.onMessage(async (context, next) => { //ORIG FROM multiturn
+            console.log('\nRunning dialog with Message Activity.');
+            await this.dialog.run(context, this.dialogState);
+            await next(); 
+            });
+
+///////////
+/*
+        this.onDialog(async (context, next) => {
+            await this.conversationState.saveChanges(context, false);
+            await this.userState.saveChanges(context, false);
+
+            await next();
+            });
+*/
 
 
 
+}
+
+
+
+
+/*
+///from https://docs.microsoft.com/en-us/azure/bot-service/bot-builder-dialog-manage-conversation-flow?view=azure-bot-service-4.0&tabs=javascript
+
+async run(context, accessor) {
+    const dialogSet = new DialogSet(accessor);
+    dialogSet.add(this);
+
+    const dialogContext = await dialogSet.createContext(context);
+    const results = await dialogContext.continueDialog();
+    if (results.status === DialogTurnStatus.empty) {
+        await dialogContext.beginDialog(this.id);
     }
+}
+*/
 
-    /**
-     * Override the ActivityHandler.run() method to save state changes after the bot logic completes.
-     */
+
+
+
     async run(context) {
         await super.run(context);
-
-        // Save state changes
         await this.userState.saveChanges(context);
-    }
+        }
+
+
 
     async sendIntroCard(context) {
         const card = CardFactory.heroCard(
@@ -154,13 +154,10 @@ this.onMembersAdded(async (context, next) => {
                     value: 'https://docs.microsoft.com/en-us/azure/bot-service/bot-builder-howto-deploy-azure?view=azure-bot-service-4.0'
                 }
             ]
-        );
+                                      );
 
-        await context.sendActivity({ attachments: [card] });
-
-    }
+                                      await context.sendActivity({ attachments: [card] });
+                                      }
 }
-
-
 
 module.exports.WelcomeBot = WelcomeBot;
