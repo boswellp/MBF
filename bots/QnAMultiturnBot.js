46cb45301a24aef0ca064c0702f079d967e30f54
Peter Boswell <petergboswell@bluewin.ch>
2020-03-24 11:49:57 +0100

    Add files via upload

--- bots/QnAMultiturnBot.js
+++ bots/QnAMultiturnBot.js
@@ -119,21 +119,25 @@ async run(context) {
 async sendIntroCard(context) {
 
         const card = CardFactory.heroCard(
-            'Information',
-            'Please take a moment to see how the FIDICchatbot works.',
+            'Welcome to Bot Framework!',
+            'Welcome to Welcome Users bot sample! This Introduction card is a great way to introduce your Bot to the user and suggest some things to get them started. We use this opportunity to recommend a few next steps for learning more creating and deploying bots.',
             ['https://aka.ms/bf-welcome-card-image'],
             [
                 {
                     type: ActionTypes.OpenUrl,
-                    title: 'Overview',
-                    value: 'https://fidic.tips/chatbot'
+                    title: 'Get an overview',
+                    value: 'https://docs.microsoft.com/en-us/azure/bot-service/?view=azure-bot-service-4.0'
                 },
                 {
-                    type: ActionTypes.MessageBack,
-                    title: 'Select a FIDIC contract',
-                    value: 'start'
+                    type: ActionTypes.OpenUrl,
+                    title: 'Ask a question',
+                    value: 'https://stackoverflow.com/questions/tagged/botframework'
+                },
+                {
+                    type: ActionTypes.OpenUrl,
+                    title: 'Learn how to deploy',
+                    value: 'https://docs.microsoft.com/en-us/azure/bot-service/bot-builder-howto-deploy-azure?view=azure-bot-service-4.0'
                 }
-
             ]
         );
 
