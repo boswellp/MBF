c0ca554c2f5c365bce36fe6ab3008eaefda8530b
Peter Boswell <petergboswell@bluewin.ch>
2020-03-24 01:11:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -57,9 +57,6 @@ const whatsAppAdapter = new TwilioWhatsAppAdapter({
 
 
 adapter.onTurnError = async (context, error) => {
-    // This check writes out errors to console log .vs. app insights.
-    // NOTE: In production environment, you should consider logging this to Azure
-    //       application insights.
     console.error(`\n [onTurnError] unhandled error: ${ error }`);
     var errorTxt = `${ error }`  
     
