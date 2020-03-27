56b8cd63a51b823327ec38c4fa588edd8d9ced8a
Peter Boswell <petergboswell@bluewin.ch>
2020-03-27 13:55:08 +0100

    Update qnamakerMultiturnDialog.js

--- dialogs/qnamakerMultiturnDialog.js
+++ dialogs/qnamakerMultiturnDialog.js
@@ -607,13 +607,13 @@ if (this._userProfileAccessor.profileName != undefined)
         
         console.log("\n608 textOrig = " + textOrig)
 
-        if (response.answers[0].answer = undefined && this._welcomedUserProperty != undefined) //first input with xxxx
+        if (response.answers[0] = undefined && this._welcomedUserProperty != undefined) //first input with xxxx
                {
                const didBotWelcomedUser = await this._welcomedUserProperty.get(stepContext.context);
                if (didBotWelcomedUser == undefined)
                    {
                    //response.answers[0].answer = {"activeLearning Enabled":false,"answers":[{"questions":["none"],"answer":"Select a FIDIC contract","context":{"isContextOnly":false}}]}  
-                   response.answers[0] = {"activeLearning Enabled":false,"answers":[]} 
+                   response = {"activeLearning Enabled":false,"answers":[]} 
                    } 
                }
 
