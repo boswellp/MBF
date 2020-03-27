ef2e9e0526f1f597284dcd2c018546bb214f3fe4
Peter Boswell <petergboswell@bluewin.ch>
2020-03-27 21:59:23 +0100

    Update qnamakerMultiturnDialog.js

--- dialogs/qnamakerMultiturnDialog.js
+++ dialogs/qnamakerMultiturnDialog.js
@@ -623,7 +623,7 @@ if (this._userProfileAccessor.profileName != undefined)
                      console.log("\n623 undefined")
                      response = {"activeLearning Enabled":false,"answers":[]} 
                      console.log("\n625 undefined") 
-                     response = {"activeLearning Enabled":false,"answers":[{"questions":["none"],"answer":"Select a FIDIC contract","score":1,"id":13446, "source":"Editorial","metadata":[],"context":{"isContextOnly":false}}]}  
+                     response = {"activeLearning Enabled":false,"answers":[{"questions":["none"],"answer":"","score":1,"id":13446, "source":"Editorial","metadata":[],"context":{"isContextOnly":false}}]}  
                      response = {"activeLearningEnabled":false,"answers":[{"questions":["c1","Stop search [c1]","cons1"],"answer":"Construction Contract 1st Ed 1999","score":1,"id":13205,"source":"Editorial","metadata":[{"name":"category","value":"contents"}],"context":{"isContextOnly":false,"prompts":[{"displayOrder":0,"qnaId":13499,"qna":null,"displayText":"c1 s"},{"displayOrder":0,"qnaId":13195,"qna":null,"displayText":"c1 0"}]}}]}
                      
                      }   
@@ -637,14 +637,14 @@ if (this._userProfileAccessor.profileName != undefined)
                           console.log("\n632 true1 : webchat on opening and Messenger on second step")
                           response = {"activeLearning Enabled":false,"answers":[]} 
                           console.log("\n634 true1") 
-                          response = {"activeLearning Enabled":false,"answers":[{"questions":["none"],"answer":"Select a FIDIC contract","score":1,"id":13446, "source":"Editorial","metadata":[],"context":{"isContextOnly":false}}]}  
+                          response = {"activeLearning Enabled":false,"answers":[{"questions":["none"],"answer":"","score":1,"id":13446, "source":"Editorial","metadata":[],"context":{"isContextOnly":false}}]}  
                          
                       
                           } else { //have a non-error response 
                           console.log("\n638 true1")
                           response = {"activeLearning Enabled":false,"answers":[]} 
                           console.log("\n640 true1") 
-                          response = {"activeLearning Enabled":false,"answers":[{"questions":["none"],"answer":"Select a FIDIC contract","score":1,"id":13446, "source":"Editorial","metadata":[],"context":{"isContextOnly":false}}]}  
+                          response = {"activeLearning Enabled":false,"answers":[{"questions":["none"],"answer":"","score":1,"id":13446, "source":"Editorial","metadata":[],"context":{"isContextOnly":false}}]}  
                           }   
                       } else {
                       console.log("\n643 2") 
