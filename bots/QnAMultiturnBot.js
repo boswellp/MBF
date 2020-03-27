288f5b612dda48a94ebbdb6f63466c5154e2398f
Peter Boswell <petergboswell@bluewin.ch>
2020-03-28 00:05:00 +0100

    Update QnAMultiturnBot.js

--- bots/QnAMultiturnBot.js
+++ bots/QnAMultiturnBot.js
@@ -76,30 +76,16 @@ class QnAMultiturnBot extends ActivityHandler {
                      //await this.sendWelcomeCard(context);
                     
                  welcomeCard.body[1].text = 'Welcome to FIDICchatbot';
-        
                  welcomeCard.body[2].text = 'The chatbot allows you to search FIDIC contracts.';
-
                  welcomeCard.body[3].text = 'Submit "start" or "help" anytime to start again and for help.';
-
                  welcomeCard.body[4].text = 'After selecting a contract, General Conditions clauses are displayed by submitting a clause number or a keyword (keywords search in the index of clauses).';
-
                  welcomeCard.body[4].text = 'After selecting a contract, clauses are displayed by submitting a clause number or a keyword.';
-
                  welcomeCard.body[5].text = 'Keywords search the index of clauses, or in the General Conditions when searching is activated.';
-
                  welcomeCard.body[5].text = 'Shortcut codes for contracts that can be submitted at any time are:\n- "c1" - Construction Contract 1st Ed 1999\n- "p1" - Plant Contract 1st Ed 1999.';
-
                  welcomeCard.body[6].text = 'FIDICchatbot is complemented by FIDICbot that suppplies messenging channels (e.g., LINE and Viber) that are not served by FIDICchatbot. Both bots are developed by Bricad Associates, Switzerland, as part of the https://FIDIC.tips initiative.';
 
-                 welcomeCard.actions[0].title = 'Privacy policy';
-
-                 welcomeCard.actions[0].url = process.env.publicResourcesUrl + 'privacy_policy_en.pdf';
-
                  await context.sendActivity({attachments: [CardFactory.adaptiveCard(welcomeCard)]});
-
-
-                 
-
+               
                 }
             }
 
