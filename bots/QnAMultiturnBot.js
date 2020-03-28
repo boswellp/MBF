de3f892a461a90b7620e3075d1702da1e686af73
Peter Boswell <petergboswell@bluewin.ch>
2020-03-28 12:14:40 +0100

    Update QnAMultiturnBot.js

--- bots/QnAMultiturnBot.js
+++ bots/QnAMultiturnBot.js
@@ -46,7 +46,7 @@ class QnAMultiturnBot extends ActivityHandler {
                  welcomeCard.body[2].text = 'The chatbot allows you to search FIDIC contracts.';
                  welcomeCard.body[3].text = 'Submit "start" or "help" anytime to start again and for help.';
                  welcomeCard.body[4].text = 'After selecting a contract, General Conditions clauses are displayed by submitting a clause number or a keyword.';
-                 welcomeCard.body[5].text = 'Keywords search the index of clauses, or in the General Conditions when searching is activated (by submitting \"c1s\" or  \"start\" -> \"c1 s\" for Construction Contract).';
+                 welcomeCard.body[5].text = 'Keywords search the index of clauses, or in the General Conditions when searching is activated (by submitting \"c1s\" or \"start\" -> \"c1 s\" for the Construction Contract).';
                  welcomeCard.body[6].text = 'Shortcut codes for contracts that can be submitted at any time are "c1" for Construction Contract 1st Ed 1999 and "p1" for Plant & Design-Build Contract 1st Ed 1999 (in the process of being uploaded).';
                  welcomeCard.body[7].text = 'FIDICchatbot is complemented by FIDICbot that suppplies messenging channels (e.g., LINE and Viber) that are not served by FIDICchatbot. Both bots are developed by Bricad Associates, Switzerland, as part of the https://FIDIC.tips initiative.';
 
@@ -74,7 +74,7 @@ class QnAMultiturnBot extends ActivityHandler {
                     
                  guidanceCard.body[1].text = 'FIDICchatbot guide';
                  guidanceCard.body[2].text = 'After selecting a contract, General Conditions clauses are displayed by submitting a clause number or a keyword.';
-                 guidanceCard.body[3].text = 'Keywords search the index of clauses, or in the General Conditions when searching is activated (by submitting \"c1s\" or  \"start\" -> \"c1 s\" for the Construction Contract).';
+                 guidanceCard.body[3].text = 'Keywords search the index of clauses, or in the General Conditions when searching is activated (by submitting \"c1s\" or \"start\" -> \"c1 s\" for the Construction Contract).';
                  guidanceCard.body[4].text = 'Shortcut codes for contracts that can be submitted at any time are "c1" for the Construction Contract 1st Ed 1999 and "p1"for the Plant & Design-Build Contract 1st Ed 1999 (in the processof being uploaded).';
                  
                  await context.sendActivity({attachments: [CardFactory.adaptiveCard(guidanceCard)]});
@@ -104,7 +104,7 @@ class QnAMultiturnBot extends ActivityHandler {
                  welcomeCard1.body[2].text = 'The chatbot allows you to search FIDIC contracts.';
                  welcomeCard1.body[3].text = 'Submit "start" or "help" anytime to start again and for help.';
                  welcomeCard1.body[4].text = 'After selecting a contract, General Conditions clauses are displayed by submitting a clause number or a keyword.';
-                 welcomeCard1.body[5].text = 'Keywords search the index of clauses, or in the General Conditions when searching is activated (by submitting \"c1s\" or  \"start\" -> \"c1 s\" for Construction Contract).';
+                 welcomeCard1.body[5].text = 'Keywords search the index of clauses, or in the General Conditions when searching is activated (by submitting \"c1s\" or \"start\" -> \"c1 s\" for the Construction Contract).';
                  welcomeCard1.body[6].text = 'Shortcut codes for contracts that can be submitted at any time are "c1" for Construction Contract 1st Ed 1999 and "p1" for Plant & Design-Build Contract 1st Ed 1999 (in the process of being uploaded).';
                  welcomeCard1.body[7].text = 'FIDICchatbot is complemented by FIDICbot that suppplies messenging channels (e.g., LINE and Viber) that are not served by FIDICchatbot. Both bots are developed by Bricad Associates, Switzerland, as part of the https://FIDIC.tips initiative.';
 
