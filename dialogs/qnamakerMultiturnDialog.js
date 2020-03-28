78d153ab02c82487e1f9455467c5f8c0ed2d4ecc
Peter Boswell <petergboswell@bluewin.ch>
2020-03-28 01:07:31 +0100

    Update qnamakerMultiturnDialog.js

--- dialogs/qnamakerMultiturnDialog.js
+++ dialogs/qnamakerMultiturnDialog.js
@@ -620,7 +620,7 @@ if (this._userProfileAccessor.profileName != undefined)
                      console.log("\n618 undefined")
                      response = {"activeLearning Enabled":false,"answers":[]} 
                      console.log("\n620 undefined") 
-                     //response  = {"activeLearning Enabled":false,"answers":[{"questions":["none"],"answer":"  ","score":1,"id":13446, "source":"Editorial","metadata":[],"context":{"isContextOnly":false}}]}  
+                     response.answers[0]  = {"activeLearning Enabled":false,"answers":[{"questions":["none"],"answer":"  ","score":1,"id":13446, "source":"Editorial","metadata":[],"context":{"isContextOnly":false}}]}  
                      } else { //have a non-error response 
                      console.log("\n623 undefined")
                      //response = {"activeLearning Enabled":false,"answers":[]} 
