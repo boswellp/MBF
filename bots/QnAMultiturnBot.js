ee7753c51c5646daeed6478201624dfc6248875a
Peter Boswell <petergboswell@bluewin.ch>
2020-03-28 22:43:08 +0100

    Update QnAMultiturnBot.js

--- bots/QnAMultiturnBot.js
+++ bots/QnAMultiturnBot.js
@@ -94,7 +94,7 @@ class QnAMultiturnBot extends ActivityHandler {
 
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
 
-                    await this.welcomedUserProperty.set(context, );
+                    await this.welcomedUserProperty.set(context, 2);
 
                     welcomeCard1.body[1].text = 'Welcome to FIDICchatbot';
                     welcomeCard1.body[2].text = 'The chatbot allows you to search FIDIC contracts.';
