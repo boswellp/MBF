b42b6e19f994125417d7a31597a68de052d721c0
Peter Boswell <petergboswell@bluewin.ch>
2020-03-30 00:32:16 +0200

    Update QnAMultiturnBot.js

--- bots/QnAMultiturnBot.js
+++ bots/QnAMultiturnBot.js
@@ -170,7 +170,7 @@ class QnAMultiturnBot extends ActivityHandler {
         const cardWelcome = CardFactory.heroCard(
             'Welcome to the FIDICchatbot',
             'The chatbot allows you to search FIDIC contracts.',
-            ['https://fidic.tips/fidicchatbot/fidicchatbot_logo_72_100.png'],
+            ['https://fidic.tips/fidicchatbot/fidicchatbot_logo_72_100.jpg'],
             [
                 {
                     type: ActionTypes.OpenUrl,
