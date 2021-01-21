7fff3fdae12dfe1af0191b7168764422be9cc228
Peter Boswell <petergboswell@bluewin.ch>
2021-01-21 23:27:55 +0100

    Update qnamakerMultiturnDialog.js

--- dialogs/qnamakerMultiturnDialog.js
+++ dialogs/qnamakerMultiturnDialog.js
@@ -621,7 +621,8 @@ console.log ("\n181 str to replace ¦ ??????? = " + str + '\n');
                        {
                        qnaMakerOptions.scoreThreshold = 0.01; 
                        qnaMakerOptions.top = 1;
-                        }
+		       qnaMakerOptions.strictFilters = [{"name": "category","value": "api"}]
+		       }
                    //}
             }
             else
@@ -630,6 +631,7 @@ console.log ("\n181 str to replace ¦ ??????? = " + str + '\n');
                  {
                       qnaMakerOptions.scoreThreshold = 0.01; 
                       qnaMakerOptions.top = 1;
+		      qnaMakerOptions.strictFilters = [{"name": "category","value": "api"}]
                  }
              }
 
