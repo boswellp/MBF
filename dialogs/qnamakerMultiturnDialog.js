120d2964a4b20c373bb377c5e55f6c1c17d45b4a
Peter Boswell <petergboswell@bluewin.ch>
2021-01-21 22:58:27 +0100

    Update qnamakerMultiturnDialog.js

--- dialogs/qnamakerMultiturnDialog.js
+++ dialogs/qnamakerMultiturnDialog.js
@@ -619,7 +619,7 @@ console.log ("\n181 str to replace ¦ ??????? = " + str + '\n');
                        }
                        else
                        {
-                       qnaMakerOptions.scoreThreshold = 90; 
+                       qnaMakerOptions.scoreThreshold = 0.99; 
                        qnaMakerOptions.top = 1;
                         }
                    //}
@@ -628,7 +628,7 @@ console.log ("\n181 str to replace ¦ ??????? = " + str + '\n');
             {
 
                  {
-                      qnaMakerOptions.scoreThreshold = 90; 
+                      qnaMakerOptions.scoreThreshold = 0.99; 
                       qnaMakerOptions.top = 1;
                  }
              }
@@ -696,7 +696,8 @@ console.log ("\n181 str to replace ¦ ??????? = " + str + '\n');
 		     
 	      console.log("\n695 RESPONSE c1 GOT USERPROFILE stepContext.context= " + JSON.stringify(stepContext.context)); 
 	      var response = await this._qnaMakerService.getAnswersRaw(stepContext.context, qnaMakerOptions);
-	      //console.log("\n695 RESPONSE c1 GOT USERPROFILE = " + JSON.stringify(response));  
+	      //console.log("\n695 RESPONSE c1 GOT USERPROFILE = " + JSON.stringify(response)); 
+	      //var qnaResults = await this.qnaMaker.getAnswers(stepContext.context, qnaMakerOptions)
 	     }		  
              else if (userProfile.indexOf('p1',0) != -1)
              { 
