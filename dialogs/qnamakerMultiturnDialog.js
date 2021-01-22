de8c044568dcf09c3b006960d3eba0dc336f042c
Peter Boswell <petergboswell@bluewin.ch>
2021-01-22 23:03:46 +0100

    Update qnamakerMultiturnDialog.js

--- dialogs/qnamakerMultiturnDialog.js
+++ dialogs/qnamakerMultiturnDialog.js
@@ -641,7 +641,7 @@ console.log ("\n181 str to replace ¦ ??????? = " + str + '\n');
              }
 	     else //not a clause
             {
-	    stepContext.context = '';
+	    stepContext.context = {};
 
                  {
                       qnaMakerOptions.scoreThreshold = 0.93; 
