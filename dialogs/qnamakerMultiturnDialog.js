bdb1a15e88e9d5aa72a92913d6f2bbdd1c1304f9
Peter Boswell <petergboswell@bluewin.ch>
2021-01-23 12:32:49 +0100

    Update qnamakerMultiturnDialog.js

--- dialogs/qnamakerMultiturnDialog.js
+++ dialogs/qnamakerMultiturnDialog.js
@@ -721,23 +721,31 @@ console.log ("\n181 str to replace ¦ ??????? = " + str + '\n');
 	      var response = await this._qnaMakerService.getAnswersRaw(stepContext.context, qnaMakerOptions);
 	      console.log("\n\n722 RESPONSE c1 GOT USERPROFILE: = " + JSON.stringify(response));
 	      if (response.answers == ''){
-		      console.log("\n724 response blank")
+		      //console.log("\n724 response blank")
 		      //response.answers[0] = 'Clause number or keyword entry in index does not exist. Please try again.'
-		      response  = {"activeLearning Enabled":false,"answers":[{"questions":["none"],"answer":"Clause number or keyword entry in index does not exist. Please try again.","score":1,"id":13446, "source":"Editorial","metadata":[],"context":{"isContextOnly":false}}]} 
-                         
+		      response  = {"activeLearning Enabled":false,"answers":[{"questions":["none"],"answer":"Clause number or keyword entry in index does not exist. Please try again.","score":1,"id":13446, "source":"Editorial","metadata":[],"context":{"isContextOnly":false}}]}  
 	              }		      
 	     }		  
              else if (userProfile.indexOf('p1',0) != -1)
              { 
              var response = await this._qnaMakerServicePlant1.getAnswersRaw(stepContext.context, qnaMakerOptions);
+	     if (response.answers == ''){
+		      response  = {"activeLearning Enabled":false,"answers":[{"questions":["none"],"answer":"Clause number or keyword entry in index does not exist. Please try again.","score":1,"id":13446, "source":"Editorial","metadata":[],"context":{"isContextOnly":false}}]}  
+	              }
 	     }		  
 	     else if (userProfile.indexOf('e1',0) != -1)
              { 
              var response = await this._qnaMakerServiceEPCT1.getAnswersRaw(stepContext.context, qnaMakerOptions);
+	     if (response.answers == ''){
+	     response  = {"activeLearning Enabled":false,"answers":[{"questions":["none"],"answer":"Clause number or keyword entry in index does not exist. Please try again.","score":1,"id":13446, "source":"Editorial","metadata":[],"context":{"isContextOnly":false}}]}  
+	              }
 	     }
 	     else
 	     {        
 	     var response = await this._qnaMakerService.getAnswersRaw(stepContext.context, qnaMakerOptions);
+	     if (response.answers == ''){
+	     response  = {"activeLearning Enabled":false,"answers":[{"questions":["none"],"answer":"Clause number or keyword entry in index does not exist. Please try again.","score":1,"id":13446, "source":"Editorial","metadata":[],"context":{"isContextOnly":false}}]}  
+	              }
 	     }
           }
           else
