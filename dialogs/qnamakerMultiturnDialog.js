eec4c966ede09778f7e65a04b138323dbb0bdf41
Peter Boswell <petergboswell@bluewin.ch>
2021-01-23 16:05:38 +0100

    Update qnamakerMultiturnDialog.js

--- dialogs/qnamakerMultiturnDialog.js
+++ dialogs/qnamakerMultiturnDialog.js
@@ -720,7 +720,7 @@ console.log ("\n181 str to replace ¦ ??????? = " + str + '\n');
 	      console.log("\n\n703 RESPONSE c1 GOT USERPROFILE: stepContext.context= " + JSON.stringify(stepContext.context)); 
 	      var response = await this._qnaMakerService.getAnswersRaw(stepContext.context, qnaMakerOptions);
 	      console.log("\n\n722 RESPONSE c1 GOT USERPROFILE: = " + JSON.stringify(response));
-	      if (response.answers == ''){
+	      if (response.answers == '' || response.answers == none || response.answers == false){
 		      //console.log("\n724 response blank")
 		      //response.answers[0] = 'Clause number or keyword entry in index does not exist. Please try again.'
 		      response  = {"activeLearning Enabled":false,"answers":[{"questions":["none"],"answer":"Clause number or keyword entry in index does not exist. Please try again.","score":1,"id":13446, "source":"Editorial","metadata":[],"context":{"isContextOnly":false}}]}  
